788f7250fae23f2216204fc71c6a0d3152c25dd0
Shanfang <shanfangzhao@gmail.com>
2018-03-29 15:36:53 -0400

    refactor shortsRouter to handle put and delete request

--- routes/shortsRoute.js
+++ routes/shortsRoute.js
@@ -4,8 +4,16 @@ var router = express.Router();
 router.get('/shorts', function (req, res, next) {
     res.send(`GET request successful. Checkout our new shorts!`);
 });
+
 router.post('/shorts', function (req, res, next) {
     res.send(`POST request successful.`);
 });
 
+router.put('/shorts', function(req, res, next) {
+    res.send(`PUT request successful.`)
+});
+
+router.delete('/shorts', function(req, res, next) {
+    res.send(`DELETE request successful.`)
+})
 module.exports = router;
